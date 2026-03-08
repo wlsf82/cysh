d431826bc18b5be72029646178ca7f65717914ae
Walmyr <wlsf82@gmail.com>
2026-03-08 02:43:04 +0100

    Add missing cypress config

--- cy.sh
+++ cy.sh
@@ -79,6 +79,7 @@ cat > cypress.config.js << 'EOF'
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  allowCypressEnv: false,
   e2e: {
     fixturesFolder: false,
     supportFile: false,
